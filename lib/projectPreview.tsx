73b81c95df4c7d71cb0290c61c56e44231668dbc
Avtorre <113251940+Avtorre@users.noreply.github.com>
2024-09-11 14:35:24 +0300

    added new project

--- lib/projectPreview.tsx
+++ lib/projectPreview.tsx
@@ -1,39 +1,46 @@
-import { ProjectCard } from "@/lib/types"
+import { ProjectCard } from "@/lib/types";
 
 export const ProjectLib = [
   {
     id: 1,
-    page: 'portfolio',
-    preview: '/images/projectImages/portfolio/portfolio_preview.png',
-    title: 'Portfolio',
-    description: "It's what you are looking at."
+    page: "portfolio",
+    preview: "/images/projectImages/portfolio/portfolio_preview.png",
+    title: "Portfolio",
+    description: "It's what you are looking at.",
   },
   {
     id: 2,
-    page: 'ghrepo-finder',
-    preview: '/images/projectImages/repo-finder/repo-finder_preview.png',
-    title: 'Repo finder',
-    description: 'Search repositories by name.'
+    page: "ghrepo-finder",
+    preview: "/images/projectImages/repo-finder/repo-finder_preview.png",
+    title: "Repo finder",
+    description: "Search repositories by name.",
   },
   {
     id: 3,
-    page: 'internetlab-task',
-    preview: '/images/projectImages/internetlab/internetlab_main.png',
-    title: 'Test assignment',
-    description: 'SPA with responsive design.'
-  },  
+    page: "internetlab-task",
+    preview: "/images/projectImages/internetlab/internetlab_main.png",
+    title: "Test assignment",
+    description: "SPA with responsive design.",
+  },
   {
     id: 4,
-    page: 'clock_re',
-    preview: '/images/projectImages/clock_re/clockProject_preview.png',
-    title: 'Clock Project',
-    description: 'Combination of several projects.'
+    page: "ecosystem_alfa-task",
+    preview: "/images/projectImages/store/store_main.png",
+    title: "Store website",
+    description: "Basic website with API calls.",
   },
   {
     id: 5,
-    page: 'picturebin',
-    preview: '/images/projectImages/picturebin/picturebin_preview.png',
-    title: 'Image hosting',
-    description: 'Fullstack project.'
+    page: "clock_re",
+    preview: "/images/projectImages/clock_re/clockProject_preview.png",
+    title: "Clock Project",
+    description: "Combination of several projects.",
   },
-] as ProjectCard[]
+  {
+    id: 6,
+    page: "picturebin",
+    preview: "/images/projectImages/picturebin/picturebin_preview.png",
+    title: "Image hosting",
+    description: "Fullstack project.",
+  },
+] as ProjectCard[];
